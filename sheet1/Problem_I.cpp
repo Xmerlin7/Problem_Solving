8a48f7af573776a63e6d5ee663b088dcd486509b
Seif Allah <saifeldarageely@gmail.com>
2024-01-07 22:29:16 +0200

    Problem_I

---
+++ sheet1/Problem_I.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    if (n < m)
+        cout << "No";
+    else
+        cout << "Yes";
+}
