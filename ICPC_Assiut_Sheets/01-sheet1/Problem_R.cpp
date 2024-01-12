1010c1a6ff3fa27c85e7919da974cf183e369637
Seif Allah <saifeldarageely@gmail.com>
2024-01-12 02:46:56 +0200

    R

---
+++ ICPC_Assiut_Sheets/01-sheet1/Problem_R.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int in, days, years, months;
+
+    cin >> in;
+    years = in / 365;
+    months = (in % 365) / 30;
+    days = ((in % 365) % 30);
+
+    cout << years << " years" << endl;
+    cout << months << " months" << endl;
+    cout << days << " days" << endl;
+}
