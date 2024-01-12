fe29996613329d7afaba5cce0af5ad11e744975a
Seif Allah <saifeldarageely@gmail.com>
2024-01-12 02:12:52 +0200

    P

--- ICPC_Assiut_Sheets/01-sheet1/Problem_P.cpp
+++ ICPC_Assiut_Sheets/01-sheet1/Problem_P.cpp
@@ -4,17 +4,16 @@ int main()
 {
     int a;
     cin >> a;
-    int n = a;
     while (a)
     {     
-        if (n < 10)
+        if (a < 10)
         {
             if (a & 0x01)
                 cout << "ODD";
             else
                 cout << "EVEN";
         }
-        n = a / 10;
+        a /= 10;
     }
 
     return (0);
