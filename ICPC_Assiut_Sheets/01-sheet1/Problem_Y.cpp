e7f9ba3709807de7adf5acebc70637c7645afff3
Seif Allah <saifeldarageely@gmail.com>
2024-01-12 22:14:35 +0200

    #

--- ICPC_Assiut_Sheets/01-sheet1/Problem_Y.cpp
+++ ICPC_Assiut_Sheets/01-sheet1/Problem_Y.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
-#include <iomanip>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-    int A, B, C, D;
 
+    int A, B, C, D;
     cin >> A >> B >> C >> D;
-    int s = A * B * C * D;
-    if (s < 10)
-        cout << 0 << s;
+
+    int product = (((A % 100) * (B % 100)) % 100 * (C % 100)) % 100 * (D % 100) % 100;
+    if (product < 10)
+        cout << 0 << product;
     else
-    {
-        s %= 100;
-        cout << s;
-    }
+        cout << product;
+
+    return 0;
 }
