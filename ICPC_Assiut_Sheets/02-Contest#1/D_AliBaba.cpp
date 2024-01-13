7b4045f826bd55a3b041ff458d3ba9ba2a2d534b
Seif Allah <darageelys40@gmail.com>
2024-01-13 19:18:54 +0200

    #

--- ICPC_Assiut_Sheets/02-Contest#1/D_AliBaba.cpp
+++ ICPC_Assiut_Sheets/02-Contest#1/D_AliBaba.cpp
@@ -4,7 +4,22 @@ using namespace std;
 #define ll long long
 int main()
 {
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    ll res1 = a + b * c;
+    ll res2 = a + b - c;
+    ll res3 = a * b + c;
+    ll res4 = a * b - c;
+    ll res5 = a - b * c;
+    ll res6 = a - b + c;
 
-    cout << "hello, Adam";
+    if ((res1 == d) || (res2 == d) || (res3 == d) || (res4 == d) || (res5 == d) || (res6 == d))
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
     return (0);
 }
