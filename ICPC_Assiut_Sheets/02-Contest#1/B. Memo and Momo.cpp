6168e8fccac2bf3fdda0a091a39c784d949ee33d
Seif Allah <saifeldarageely@gmail.com>
2024-01-13 15:53:36 +0200

    B

---
+++ ICPC_Assiut_Sheets/02-Contest#1/B. Memo and Momo.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long a, b, k;
+    std::cin >> a >> b >> k;
+
+    if (a % k == 0 && b % k == 0)
+    {
+        cout << "Both";
+    }
+    else if (a % k == 0)
+    {
+        cout << "Memo";
+    }
+    else if (b % k == 0)
+    {
+        cout << "Momo";
+    }
+    else
+    {
+        cout << "No One";
+    }
+    return (0);
+}
