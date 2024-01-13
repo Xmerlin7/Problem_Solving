c387a27dfdb7bf412973d70691b8b0ae7738cf42
Seif Allah <saifeldarageely@gmail.com>
2024-01-13 15:48:51 +0200

    A

--- ICPC_Assiut_Sheets/02-Contest#1/A.winter_sale.cpp
+++ ICPC_Assiut_Sheets/02-Contest#1/A.winter_sale.cpp
@@ -1,8 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main ()
+int main()
 {
+    int X, P;
+    cin >> X >> P;
+
+    double discountedPrice = static_cast<double>(P);
+    double originalPrice = discountedPrice / (1 - (static_cast<double>(X) / 100));
+
+    //? Round up to two decimal places 23.53 not 23.52
+     originalPrice = ceil(originalPrice * 100) / 100;
+
+    cout << fixed << setprecision(2) << originalPrice << endl;
 
     return (0);
 }
