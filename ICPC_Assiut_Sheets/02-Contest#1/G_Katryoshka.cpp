c438ca3b2fd7cdb0550edb1f9a139a3654ea8721
Seif Allah <darageelys40@gmail.com>
2024-01-16 17:41:45 +0200

    #

--- ICPC_Assiut_Sheets/02-Contest#1/G_Katryoshka.cpp
+++ ICPC_Assiut_Sheets/02-Contest#1/G_Katryoshka.cpp
@@ -5,6 +5,14 @@ int main()
 {
     long long eye, mouth, body;
     cin >> eye >> mouth >> body;
+    //! note: if u try solve it with loops u will get
+    //! t time limit*/
 
+    if ((eye == 0) || (body == 0))
+        cout << 0;
+    else if ((mouth == 0) && (eye > 0) && (body > 0))
+    {
+        
+    }
     return (0);
 }
