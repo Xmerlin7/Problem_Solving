48850100b41dd5a077a75390d0cefdf9bff0df8b
Seif Allah <darageelys40@gmail.com>
2024-01-16 19:53:50 +0200

    #

--- ICPC_Assiut_Sheets/02-Contest#1/I_Lucky_Numbers.cpp
+++ ICPC_Assiut_Sheets/02-Contest#1/I_Lucky_Numbers.cpp
@@ -3,9 +3,14 @@ using namespace std;
 
 int main()
 {
-    double n, k, a;
-    cin >> n >> k >> a;
+    int a;
+    cin >> a;
+    int fDigit = a % 10;
+    int lDigit = (a / 10);
 
-
+    if ((fDigit / lDigit) == 0 || (lDigit / fDigit) == 0)
+        cout << "YES";
+    else
+        cout << "NO";
     return 0;
 }
