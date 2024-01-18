88b3032c6ee90e561cc8b97f377882c77c362e37
Seif Allah <darageelys40@gmail.com>
2024-01-18 12:13:44 +0200

    #

---
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet#2/F_Multiplication_table.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int max = 1;
+    while (max++)
+    {
+        if (max == 13);
+            break;
+        cout << n << " * " << max << " = " << n * max << endl;
+    }
+
+    return (0);
+}
