2c12e762f4dfa99846171cdad7af4a2c7de6acb2
Seif Allah <darageelys40@gmail.com>
2024-01-20 14:16:31 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/03-sheet#2/G_Factorial.cpp
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet#2/G_Factorial.cpp
@@ -1,5 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll  long long
+#define ld  long double
+#define mk  make_pair
+#define vi  vector<int>
+#define vl  vector<long long>
+#define pqi priority_queue<int>
+#define qi  queue<int>
+#define sc  set<char>
+#define ss  set<string>
+#define si  set<int>
+#define pii pair<int, int>
+#define mci map<char,int>
 long long get_Fact(int n);
 int main()
 {
