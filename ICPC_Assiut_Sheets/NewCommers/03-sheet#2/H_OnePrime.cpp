a94ff20986001ddc5c1a2ff59f546c39488060df
Seif Allah <darageelys40@gmail.com>
2024-01-20 14:32:24 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/03-sheet#2/H_OnePrime.cpp
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet#2/H_OnePrime.cpp
@@ -27,7 +27,7 @@ bool isPrime (int n)
     if ( n == 0 || n == 1)
     isPrime = false;
 
-    for (int i = 2; i <= (n))
+    for (int i = 2; i <= (n / 2); i++)
     {
         if (n % i == 0)
         {
