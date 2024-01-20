697d407947160329650693a9afe06671650d3d89
Seif Allah <darageelys40@gmail.com>
2024-01-20 14:23:12 +0200

    #

---
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet#2/H_OnePrime.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mk make_pair
+#define vi vector<int>
+#define vl vector<long long>
+#define pqi priority_queue<int>
+#define qi queue<int>
+#define sc set<char>
+#define ss set<string>
+#define si set<int>
+#define pii pair<int, int>
+#define mci map<char, int>
+ll get_Fact(int n);
+int main(void)
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        cout << get_Fact(arr[i]) << endl;
+    }
+    return (0);
+}
+ll get_Fact(int n)
+{
+    long long fact;
+    //? both 1 or zero will wrok, but (1) reduce 1 stack frame 
+    //? so it's better to reduce memory*/
+    if (n == 1)
+        return (1);
+    return (n * get_Fact(n - 1));
+}
