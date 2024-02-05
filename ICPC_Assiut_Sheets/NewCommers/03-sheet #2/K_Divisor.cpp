cc84fa64753a34fa7ff67860734a750d07fe8432
Seif Allah <saifeldarageely@gmail.com>
2024-02-05 19:45:01 +0200

    #

---
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet #2/K_Divisor.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mk make_pair
+#define vi vector<int>
+#define vl vector<long long>
+#define pqi priority_queue<int>
+#define qi queue<int>
+#define sc set<char>
+#define ss set<string>
+#define si set<int>
+#define pii pair<int, int>
+#define mci map<char, int>
+
+bool isPrime(int n);
+int main(void)
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        
+        if ((n % i) == 0)
+            cout << i << endl;
+    }
+
+    return (0);
+}
