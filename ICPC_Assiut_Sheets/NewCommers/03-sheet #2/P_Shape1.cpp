617de05a3e1420544d7a86939e54fa7ec3cb6cd3
Seif Allah <saifeldarageely@gmail.com>
2024-02-07 15:12:35 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/03-sheet #2/P_Shape1.cpp
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet #2/P_Shape1.cpp
@@ -1,37 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-int digit_numbers(long long n);
+
 int main()
 {
     int n;
     cin >> n;
-    int digit;
-    long long x[n], num, rev[100];
-    for (int i = 0; i < n; i++)
-        cin >> x[i];
+
     for (int i = 0; i < n; i++)
     {
-        num = x[i];
-        for (int j = digit_numbers(num); j > 0; j--)
+        for (int j = 0; j < (n - i); j++)
         {
-            num %= 10;
-            rev[j] = num;
+            cout << "*";
         }
-        for (int j = digit_numbers(num); j > 0; j--)
-        {
-
-            cout << rev[j] << " ";
-        }
+        cout << endl;
     }
     return (0);
-}
-int digit_numbers(long long n)
-{
-    int ctr = 0;
-    while(n)
-    {
-        n /= 10;
-        ctr++;
-    }
-    return(ctr);
 }
