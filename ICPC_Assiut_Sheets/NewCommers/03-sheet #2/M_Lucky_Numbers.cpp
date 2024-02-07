a8c0c11dec5c17ec2564bd5439c55238dfae8ab4
Seif Allah <saifeldarageely@gmail.com>
2024-02-07 11:05:17 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/03-sheet #2/M_Lucky_Numbers.cpp
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet #2/M_Lucky_Numbers.cpp
@@ -18,32 +18,11 @@ int main()
     int n1, n2, digit = 0, temp, imagee = 0;
     cin >> n1 >> n2;
 
-    bool isLucky = false, stop = true;
+    bool isLucky = false;
     for (int i = n1; i <= n2; i++)
     {
         temp = i;
         imagee = i;
-        while (temp > 0)
-        {
-            digit = temp % 10;
-            temp /= 10;
-            if (digit == 7 || digit == 4)
-            {
-                isLucky = true;
-                if (temp == 0)
-                {
-                    stop = false;
-                    cout << imagee << " ";
-                }
-            }
-            else
-            {
-                isLucky = false;
-                break;
-            }
-        }
-    }
 
-    if (isLucky == false && stop == true)
-        cout << -1;
+    }
 }
