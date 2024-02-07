24aa316c669177e8504130e31d73f39a4a12904a
Seif Allah <saifeldarageely@gmail.com>
2024-02-07 14:41:04 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/03-sheet #2/P_Shape1.cpp
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet #2/P_Shape1.cpp
@@ -9,7 +9,7 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i + 1; j++)
+        for (int j = 0; j < (n - i); j++)
         {
             cout << "*";
         }
