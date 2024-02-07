0064719a6443ea9d8c945d8af28a16df08b29f34
Seif Allah <saifeldarageely@gmail.com>
2024-02-07 14:29:22 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/03-sheet #2/N_Numbers_Histogram.cpp
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet #2/N_Numbers_Histogram.cpp
@@ -5,10 +5,20 @@ int main()
 {
     int n;
     char ch;
-    cin >> ch>> n;
+    cin >> ch >> n;
     int x[n];
-    for (int i = 0; i< n; i++)
+    for (int i = 0; i < n; i++)
         cin >> x[i];
-    
+    if ((ch == '+') || (ch == '*') || (ch == '-') || (ch == '/'))
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < x[i]; j++)
+            {
+                cout << ch << " ";
+            }
+            cout << endl;
+        }
+    }
     return (0);
 }
