6b03d32e942573cfa6bad260de6c5ed631c222a9
Seif Allah <saifeldarageely@gmail.com>
2024-02-07 14:35:32 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/03-sheet #2/O_Pyramids.cpp
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet #2/O_Pyramids.cpp
@@ -4,19 +4,16 @@ using namespace std;
 int main()
 {
     int n;
-    char ch;
-    cin >> ch >> n;
-    int x[n];
-    for (int i = 0; i < n; i++)
-        cin >> x[i];
 
-        for (int i = 0; i < n; i++)
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
         {
-            for (int j = 0; j < x[i]; j++)
-            {
-                cout << "*";
-            }
-            cout << endl;
+            cout << "*";
         }
+        cout << endl;
+    }
     return (0);
 }
