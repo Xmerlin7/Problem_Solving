199cd478d80433a70abbb1aeb93a10e8d06adb07
Seif Allah <saifeldarageely@gmail.com>
2024-02-08 17:51:35 +0200

    #

---
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet #2/z_Three_NUmbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short int k, s;
+    int sum;
+
+    int ctr = 0;
+    cin >> k >> s;
+
+    for (int x = 0; x <= k; x++)
+    {
+        for (int y = 0; y <=)
+    }
+    return 0;
+}
