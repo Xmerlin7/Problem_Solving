8db438146cfd6321a289445bae41ae40ea4542f9
Seif Allah <saifeldarageely@gmail.com>
2024-02-08 17:30:37 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/03-sheet #2/S_Sum_Of_ODD.cpp
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet #2/S_Sum_Of_ODD.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	short int m, n;
-	int sum;
+int main()
+{
+    short int m, n;
+    int sum;
 
-      int count;
-      cin>> count;
-	for (int i=0; i<count; i++){
-		cin >> m >> n;
-		sum = 0;
-		
-		//if(m<=0 || n<=0)
-		//	break;
-		
-		if(m>n){
-			int temp = m;
-                  m= n;
-                  n=temp;     
-		}
-		
-		for(int i = m+1; i<=n-1; ++i){
-			if (i%2 !=0) sum+=i;
-		}
-		cout<<sum<<"\n";
-	} 	
-	return 0;
+    int count;
+    cin >> count;
+    for (int i = 0; i < count; i++)
+    {
+        cin >> m >> n;
+        sum = 0;
+
+        for (int i = min(m, n) + 1; i <= max(m, n) - 1; ++i)
+        {
+            if (i % 2 != 0)
+                sum += i;
+        }
+        cout << sum << endl;
+    }
+    return 0;
 }
