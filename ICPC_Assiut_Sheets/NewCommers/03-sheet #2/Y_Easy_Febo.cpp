a05f0f38cab9faebd6668bbb4508004592810ff2
Seif Allah <saifeldarageely@gmail.com>
2024-02-08 18:23:29 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/03-sheet #2/Y_Easy_Febo.cpp
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet #2/Y_Easy_Febo.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+using namespace std;
+void fib(int n)
+{
+    int f[n];
+    int i;
+    f[0] = 0;
+    f[1] = 1;
+    for (i = 2; i < n; i++)
+    {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+    for (i = 0; i < n; i++)
+    {
+        cout << f[i] << " ";
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    fib(n);
+    return 0;
+}
+
+/*#include <iostream>
 
 using namespace std;
 
@@ -6,18 +31,17 @@ int main()
 {
     int n;
     cin >> n;
-
+    int a = 0, b = 1, c;
     if (n == 0)
     {
         cout << 0 << endl;
     }
     else if (n == 1)
     {
-        cout << 0 << " " << 1 << endl;
+        cout << a << " " << b << endl;
     }
     else
     {
-        int a = 0, b = 1, c;
         cout << a << " " << b << " ";
         for (int i = 2; i < n; ++i)
         {
@@ -30,4 +54,4 @@ int main()
     }
 
     return 0;
-}
+}*/
