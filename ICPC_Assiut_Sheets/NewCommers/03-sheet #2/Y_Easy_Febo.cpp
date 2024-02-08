798c65c1f2aa1aece56b4643123fda999c3d04e9
Seif Allah <saifeldarageely@gmail.com>
2024-02-08 17:56:46 +0200

    #

---
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet #2/Y_Easy_Febo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int sum;
+
+    int count;
+    cin >> count;
+    for (int i = 0; i < count; i++)
+    {
+
+        cout << sum << endl;
+    }
+    return 0;
+}
