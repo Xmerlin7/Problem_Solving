b014dbce2bbc2ec9473094538f88b8cd86094a85
Seif Allah <saifeldarageely@gmail.com>
2024-02-08 17:59:39 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/03-sheet #2/Y_Easy_Febo.cpp
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet #2/Y_Easy_Febo.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main()
 {
     int n;
+    int i = 0, k = 1;
+    cout << i << " " << k << " ";
     for (int i = 0; i < n; i++)
     {
         
-        cout << i << endl;
+        
     }
     return 0;
 }
