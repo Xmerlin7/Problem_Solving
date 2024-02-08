f3d5de1e7c76e298cbdff609fd0b02fd18add33d
Seif Allah <saifeldarageely@gmail.com>
2024-02-08 18:18:29 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/03-sheet #2/Y_Easy_Febo.cpp
+++ ICPC_Assiut_Sheets/NewCommers/03-sheet #2/Y_Easy_Febo.cpp
@@ -5,17 +5,8 @@ using namespace std;
 int main()
 {
     int n;
-
-    cout << "Enter the number of terms in the Fibonacci sequence (positive integer): " << endl;
     cin >> n;
 
-    if (n < 0)
-    {
-        cout << "Invalid input: Please enter a positive integer." << endl;
-        return 1; // Indicate error
-    }
-
-    // Calculate and print Fibonacci numbers
     if (n == 0)
     {
         cout << 0 << endl;
