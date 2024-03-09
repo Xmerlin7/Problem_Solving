258c0c0d7ba9e206a3fad43966e35a5e805bda0e
Seif Allah <saifeldarageely@gmail.com>
2024-03-09 23:52:47 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/04-Contest #2/A_Timon_Pomba.cpp
+++ ICPC_Assiut_Sheets/NewCommers/04-Contest #2/A_Timon_Pomba.cpp
@@ -4,22 +4,13 @@ using namespace std;
 int main()
 {
 
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            if ((i == j) && (i != (n / 2)) && (j != (n / 2))){
-                cout << "\\" << endl;
-            }
-            else if((i != (n / 2)) && (j != (n / 2))){
-                cout << "x";
-            }else if (j != (n / 2) && (n - 1) - j){
-                cout << "/";
-            }
-            else {
-                cout << "*";
-            }
-        }
-    }
+    int a, b;
+    cin >> a >> b;
+
+    if (a - b >= 0)
+        cout << a - b;
+    else
+        cout << 0;
+
     return 0;
 }
