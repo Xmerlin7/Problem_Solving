104e7683e82a89ffb0420fabf85995b433e51581
Seif Allah <saifeldarageely@gmail.com>
2024-03-09 22:59:53 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/04-Contest #2/A_Timon_Pomba.cpp
+++ ICPC_Assiut_Sheets/NewCommers/04-Contest #2/A_Timon_Pomba.cpp
@@ -3,17 +3,12 @@ using namespace std;
 
 int main()
 {
-    int n;
+    long long a, b;
 
-    cin >> n;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i + 1; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
+    cin >> a, b;
+    if ((a - b) > 0)
+        cout << a - b ;
+    else
+        cout << 0;
     return (0);
 }
