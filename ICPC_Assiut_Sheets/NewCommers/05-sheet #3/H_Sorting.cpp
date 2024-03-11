d3f7088b2daba6276b9d29b7e7cc4fd38f2a60a4
Seif Allah <saifeldarageely@gmail.com>
2024-03-12 00:52:30 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/H_Sorting.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/H_Sorting.cpp
@@ -9,16 +9,29 @@ int main()
     int arr[n];
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    //! Big O: O(n)
-    for (int i = 0; i < n; i++)
+    //! Big O: O(n^2)
+    for (int i = 0; i < n - 1; i++)
     {
-        if (arr[i + 1] < arr[i])
+        bool swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++)
         {
-            swap(arr[i + 1], arr[i]);
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        //? If no swaps occurred, the array is already sorted
+        if (!swapped)
+        {
+            break;
         }
     }
     for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
+    }
     return 0;
 }
 void swap(int &x, int &c)
