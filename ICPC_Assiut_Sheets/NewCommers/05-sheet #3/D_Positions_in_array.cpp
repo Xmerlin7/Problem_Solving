450fc73bce874e6f84dfbb32d5b4bf0dcb2e40f1
Seif Allah <saifeldarageely@gmail.com>
2024-03-11 23:51:35 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/D_Positions_in_array.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/D_Positions_in_array.cpp
@@ -7,21 +7,22 @@ int main()
     int n, x, found = 0;
     cin >> n;
 
-    int arr[n];
+    int arr[n], ferqArray[n] = {0};
     for (int i = 0; i < n; i++)
         cin >> arr[i];
     //! Big O: O(n)
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] > 0)
-            arr[i] = 1;
-
-        else if (arr[i] < 0)
-            arr[i] = 2;
+        if (arr[i] <= 10)
+            ferqArray[i] = 1;
     }
 
     for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+    {
+        if (ferqArray[i])
+            cout << "A[" << i << "]"
+                 << " = " << arr[i] << endl;
+    }
 
     return 0;
 }
