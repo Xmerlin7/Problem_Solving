7fe2080a428c0aa00b6febe503aae4312fa17dc7
Seif Allah <saifeldarageely@gmail.com>
2024-03-12 00:11:31 +0200

    #

---
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/G_Palindrome_Array.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n, lowest = 2000, x = 0;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    //! Big O: O(n)
+    for (int i = n - 1; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
