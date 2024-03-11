6075feda315d3d5ff027e03af29ecf7befab0273
Seif Allah <saifeldarageely@gmail.com>
2024-03-12 00:09:41 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/F_Reverse.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/F_Reverse.cpp
@@ -11,7 +11,7 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> arr[i];
     //! Big O: O(n)
-    for (int i = n - 1; i >= 0; i++)
+    for (int i = n - 1; i >= 0; i--)
     {
         cout << arr[i] << " ";
     }
