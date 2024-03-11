032eadbd68ce5c97f0e104183f33b0cd37913773
Seif Allah <saifeldarageely@gmail.com>
2024-03-12 00:36:17 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/H_Sorting.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/H_Sorting.cpp
@@ -10,7 +10,7 @@ int main()
     for (int i = 0; i < n; i++)
         cin >> arr[i];
     //! Big O: O(n)
-    for (int i = 0; i > n; i++)
+    for (int i = 0; i < n; i++)
     {
         if (arr[i + 1] < arr[i])
         {
