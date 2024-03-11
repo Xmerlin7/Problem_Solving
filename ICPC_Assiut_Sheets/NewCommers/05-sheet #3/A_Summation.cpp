821f5511180c288a38afd0802ba88e6106edcced
Seif Allah <saifeldarageely@gmail.com>
2024-03-11 23:00:55 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/A_Summation.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/A_Summation.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main()
 {
 
-    int n, a;
-    cin >> a ;
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
     
-
-
     return 0;
 }
