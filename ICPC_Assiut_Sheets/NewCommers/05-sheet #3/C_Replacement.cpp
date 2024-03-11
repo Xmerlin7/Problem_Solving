bd0399279b3425450b6fcb2d3c84e7f66ed8922c
Seif Allah <saifeldarageely@gmail.com>
2024-03-11 23:42:04 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/C_Replacement.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/C_Replacement.cpp
@@ -16,17 +16,14 @@ int main()
     for (int i = 0; i < n; i++)
     {
         if (arr[i] > 0)
-        {
             arr[i] = 1;
-        }
+
         else if (arr[i] < 0)
             arr[i] = 2;
     }
 
     for (int i = 0; i < n; i++)
-    {
         cout << arr[i] << " ";
-    }
 
     return 0;
 }
