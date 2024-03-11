e48139caa29a158014205f035e29fb000621ab3c
Seif Allah <saifeldarageely@gmail.com>
2024-03-11 23:14:31 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/B_Searching.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/B_Searching.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main()
 {
 
-    int n, x;
+    int n, x, mid, start, end;
     cin >> n;
     long long a[n];
     for (int i = 0; i < n; i++)
         cin >> a[i];
     cin >> x;
+
     return 0;
 }
