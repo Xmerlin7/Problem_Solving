f32f406428b81ac3148b9e7765fed360716ad5e0
Seif Allah <saifeldarageely@gmail.com>
2024-03-11 23:34:11 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/B_Searching.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/B_Searching.cpp
@@ -4,32 +4,57 @@ using namespace std;
 int main()
 {
 
-    int n, x, mid, start = 0, end = n - 1;
+    int n, x, found = 0;
     cin >> n;
-    long long arr[n];
+
+    int arr[n];
     for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    cin >> x;
-    while (start <= end)
     {
-        mid = (start + end) / 2;
-        if (x > arr[mid])
+        cin >> arr[i];
+    }
+    cin >> x;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
         {
-            start = mid + 1;
-        }
-        else
-            end = mid - 1;
-        if (x == arr[mid])
-        {
-            cout << start;
-            break;
-        }
-        else
-        {
-            cout << -1;
+            cout << i;
+            found = 1;
             break;
         }
     }
+
+    if (found == 0)
+        cout << -1;
+
+
+    //? Binary search only works if the array is sorted
+
+    // int n, x, mid, start = 0, end = n - 1;
+    // cin >> n;
+    // long long arr[n];
+    // for (int i = 0; i < n; i++)
+    //     cin >> arr[i];
+    // cin >> x;
+    // while (start <= end)
+    // {
+    //     mid = (start + end) / 2;
+    //     if (x > arr[mid])
+    //     {
+    //         start = mid + 1;
+    //     }
+    //     else
+    //         end = mid - 1;
+    //     if (x == arr[mid])
+    //     {
+    //         cout << start;
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         cout << -1;
+    //         break;
+    //     }
+    // }
 
     return 0;
 }
