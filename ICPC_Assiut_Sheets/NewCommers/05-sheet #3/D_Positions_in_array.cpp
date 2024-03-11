ad007bb53118693bd4b38af6ed4185433481f797
Seif Allah <saifeldarageely@gmail.com>
2024-03-12 00:07:43 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/D_Positions_in_array.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/D_Positions_in_array.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main()
 {
-
+ 
     int n, lowest = 2000, x = 0;
     cin >> n;
-
+ 
     int arr[n];
     for (int i = 0; i < n; i++)
         cin >> arr[i];
@@ -20,6 +20,6 @@ int main()
         }
     }
     cout << lowest << " " << x;
-
+ 
     return 0;
 }
