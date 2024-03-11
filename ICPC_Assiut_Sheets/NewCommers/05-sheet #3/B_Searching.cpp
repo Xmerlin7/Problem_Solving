732c28430bbffa82c00ba7cb08a7c08bcd316cc7
Seif Allah <saifeldarageely@gmail.com>
2024-03-11 23:18:48 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/B_Searching.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/B_Searching.cpp
@@ -6,14 +6,14 @@ int main()
 
     int n, x, mid, start = 0, end = n - 1;
     cin >> n;
-    long long a[n];
+    long long arr[n];
     for (int i = 0; i < n; i++)
-        cin >> a[i];
+        cin >> arr[i];
     cin >> x;
     while (start < end)
     {
         mid = n / 2;
-        if (x > mid)
+        if (x > arr[mid])
         {
             start = mid + 1;
         }
