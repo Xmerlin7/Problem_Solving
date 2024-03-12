9c804b2803b71a46908ee81dd8fc8cf3e9713441
Seif Allah <saifeldarageely@gmail.com>
2024-03-12 15:14:32 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/I_Smallest_Pair.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/I_Smallest_Pair.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int n, t, smallest;
+    int n, t, smallest = 20000;
     cin >> t, n;
     int arr[n];
 
@@ -14,7 +14,7 @@ int main()
         //! Big O: O(n ^ 2)
         for (int i = 1; i <= n; i++)
         {
-            for (int j = 1; i < n; j++)
+            for (int j = 1; j < n; j++)
             {
                 if (smallest > (arr[i] + arr[j] + j - i))
                     smallest = (arr[i] + arr[j] + j - i);
