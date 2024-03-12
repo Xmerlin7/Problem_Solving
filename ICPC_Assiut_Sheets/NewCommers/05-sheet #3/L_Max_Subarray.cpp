2647dd69f42e1a500eb578e26efb1fb6e791aaf4
Seif Allah <saifeldarageely@gmail.com>
2024-03-12 18:50:42 +0200

    #

---
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/L_Max_Subarray.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int counter;
+    cin >> counter;
+    while (counter--)
+    {
+        int size;
+        cin >> size;
+        int arr[size];
+        for (int i = 0; i < size; i++)
+        {
+            cin >> arr[i];
+            cout << arr[i] << " ";
+        }
+        int i = 0;
+        int z = 0;
+        int ma;
+        while (true)
+        { 
+
+        }
+        cout << endl;
+    }
+}
