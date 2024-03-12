e8e9fb9eba5b910f02bec2bce0fdcbe98fafdae1
Seif Allah <saifeldarageely@gmail.com>
2024-03-12 15:24:04 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/I_Smallest_Pair.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/I_Smallest_Pair.cpp
@@ -22,7 +22,7 @@ int main()
 
         for (int i = 1; i < n; i++)
         {
-            for (int j = 1; j < n; j++)
+            for (int j = 2; j < n; j++)
             {
                 int sum = arr[i] + arr[j] + j - i;
                 if (sum < smallestFound)
