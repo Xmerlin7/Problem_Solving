b538a0faf9e64cbf40d792f9f0ba3f1e2ee6f0cf
Seif Allah <saifeldarageely@gmail.com>
2024-03-12 14:58:01 +0200

    #

---
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/I_Smalest_Pair.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+void swap(int &x, int &c);
+int main()
+{
+    int n;
+    cin >> n;
+    bool flag = true;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    //! Big O: O(n^2)
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        //? If no swaps occurred, the array is already sorted
+        if (!swapped)
+        {
+            break;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+void swap(int &x, int &c)
+{
+    int temp = x;
+    x = c;
+    c = temp;
+}
