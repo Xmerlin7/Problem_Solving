7d8593458844f9b4c4b1bd683748309ed7015288
Seif Allah <saifeldarageely@gmail.com>
2024-03-12 14:59:58 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/I_Smalest_Pair.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/I_Smalest_Pair.cpp
@@ -3,37 +3,44 @@ using namespace std;
 void swap(int &x, int &c);
 int main()
 {
-    int n;
-    cin >> n;
+    int n, t;
+    cin >> n, t;
     bool flag = true;
     int arr[n];
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    //! Big O: O(n^2)
-    for (int i = 0; i < n - 1; i++)
-    {
-        bool swapped = false;
 
-        for (int j = 0; j < n - i - 1; j++)
+    while (t)
+    {
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        //! Big O: O(n^2)
+        for (int i = 0; i < n - 1; i++)
         {
-            if (arr[j] > arr[j + 1])
+            bool swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++)
             {
-                swap(arr[j], arr[j + 1]);
-                swapped = true;
+                if (arr[j] > arr[j + 1])
+                {
+                    swap(arr[j], arr[j + 1]);
+                    swapped = true;
+                }
+            }
+            //? If no swaps occurred, the array is already sorted
+            if (!swapped)
+            {
+                break;
             }
         }
-        //? If no swaps occurred, the array is already sorted
-        if (!swapped)
+        for (int i = 0; i < n; i++)
         {
-            break;
+            cout << arr[i] << " ";
         }
-    }
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
+        t--;
     }
     return 0;
 }
+
+
 void swap(int &x, int &c)
 {
     int temp = x;
