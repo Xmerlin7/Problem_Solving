85261b119cf15334539e01c3f6d6b750dc3b627f
Seif Allah <saifeldarageely@gmail.com>
2024-03-15 07:32:51 +0200

    #

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/M_Replace_MinMax.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/M_Replace_MinMax.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main()
 {
 
-    int maxx = numeric_limits<int>::max();
-    int min = numeric_limits<int>::min();
-    int size;
+    int maxx = numeric_limits<int>::min();
+    int minn = numeric_limits<int>::max();
+    int size, iMaxSaver, iMinSaver;
     cin >> size;
     int arr[size];
     for (int i = 0; i < size; i++)
@@ -13,8 +13,26 @@ int main()
         cin >> arr[i];
     }
 
-    while (true)
+    for (int i = 0; i < size; i++)
     {
+        if (maxx < arr[i])
+        {
+            maxx = arr[i];
+            iMaxSaver = i
+        }
+        if (minn > arr[i])
+        {
+            minn = arr[i];
+            iMinSaver = i;
+        }
     }
-    cout << endl;
+    swap(arr, iMinSaver, iMaxSaver);
+    for (auto &x : arr)
+        cout << x << "";
+}
+void swap(int *arr, int iMinSaver, int iMaxSaver)
+{
+    int temp = arr[iMaxSaver];
+    arr[iMaxSaver] = arr[iMinSaver];
+    arr[iMinSaver] = temp;
 }
