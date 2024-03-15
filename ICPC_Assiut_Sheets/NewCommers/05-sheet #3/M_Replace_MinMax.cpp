1593b669fef8096c8a59bfca32369b82c2bd0a97
Seif Allah <saifeldarageely@gmail.com>
2024-03-15 07:20:50 +0200

    #

---
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/M_Replace_MinMax.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int maxx = numeric_limits<int>::max();
+    int min = numeric_limits<int>::min();
+    int size;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    while (true)
+    {
+    }
+    cout << endl;
+}
