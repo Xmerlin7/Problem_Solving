6cec3e5686f4348ebb09f54de16e4471fe5d96e3
Seif Allah <saifeldarageely@gmail.com>
2024-03-16 17:21:59 +0200

    R

--- ICPC_Assiut_Sheets/NewCommers/05-sheet #3/R_Permutaions.cpp
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/R_Permutaions.cpp
@@ -10,11 +10,12 @@ int main()
     {
         cin >> arr1[i];
     }
-    sort(arr1, arr1 + size);
+    
     for (int i = 0; i < size; i++)
     {
         cin >> arr2[i];
     }
+    sort(arr1, arr1 + size);
     sort(arr2, arr2 + size);
     int flag = 0;
     for (int i = 0; i < size; i++)
