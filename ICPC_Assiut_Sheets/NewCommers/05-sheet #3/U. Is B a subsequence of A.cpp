dbf6993c7d079e8db75207b22c578a4dc7185552
Seif Allah <saifeldarageely@gmail.com>
2024-03-16 22:46:08 +0200

    creating loops

---
+++ ICPC_Assiut_Sheets/NewCommers/05-sheet #3/U. Is B a subsequence of A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int sizeA, sizeB;
+    cin >> sizeA >> sizeB;
+    long long arrA[sizeA], arrB[sizeB];
+    for (int i = 0; i < sizeA; i++)
+    {
+        cin >> arrA[i];
+    }
+    for (int i = 0; i < sizeB; i++)
+    {
+        cin >> arrB[i];
+    }
+}
