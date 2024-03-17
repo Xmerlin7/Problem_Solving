7863378dd7f4c5e6dca9346446caedec569da30a
Seif Allah <saifeldarageely@gmail.com>
2024-03-18 00:48:18 +0200

    Another solution instead of summision

--- ICPC_Assiut_Sheets/NewCommers/07-sheet #4/H_Good_Bad.cpp
+++ ICPC_Assiut_Sheets/NewCommers/07-sheet #4/H_Good_Bad.cpp
@@ -5,27 +5,33 @@ using namespace std;
 
 int main()
 {
-    string num;
-    bool flag = false;
     int testCases;
     cin >> testCases;
+
     while (testCases--)
     {
+        string num;
         cin >> num;
+
+        bool foundGood = false;
         for (int i = 0; i < num.length() - 2; i++)
         {
-            if (((num[i] + num[i + 1] + num[i + 2]) - (3 * 48)) == 1 ||
-                ((num[i - 1] + num[i] + num[i + 1]) - (3 * 48)) == 1)
+            if ((num[i] == '0' && num[i + 1] == '1' && num[i + 2] == '0') ||
+                (num[i] == '1' && num[i + 1] == '0' && num[i + 2] == '1'))
             {
-
-                flag = true;
+                foundGood = true;
                 break;
             }
         }
-        if (flag == false)
-            cout << "Bad" << endl;
-        else
+
+        if (foundGood)
+        {
             cout << "Good" << endl;
+        }
+        else
+        {
+            cout << "Bad" << endl;
+        }
     }
 
     return 0;
