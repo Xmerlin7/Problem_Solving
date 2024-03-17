6be5d66bedac98d1b9724128c4e307c85a7595ef
Seif Allah <saifeldarageely@gmail.com>
2024-03-17 17:51:26 +0200

    Solved

--- ICPC_Assiut_Sheets/NewCommers/07-sheet #4/E_Count.cpp
+++ ICPC_Assiut_Sheets/NewCommers/07-sheet #4/E_Count.cpp
@@ -6,9 +6,13 @@ using namespace std;
 int main()
 {
     string num;
+    long long sum = 0;
     cin >> num;
+    for (int i = 0; i < num.size(); i++)
+    {
+        sum += num[i] - 48;
+    }
 
-
-
+    cout << sum <<endl;
     return 0;
 }
